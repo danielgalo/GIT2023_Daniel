75b9ca63ab24cdd51e1e785ab8667e7950d4d2f7
Daniel Galo <a_daniel.galo.vega@iespablopicasso.es>
2023-02-03 13:23:05 +0100

    Añado proyecto con clase

---
+++ src/Vehiculo.java
@@ -0,0 +1,17 @@
+
+public class Vehiculo {
+
+  private String marca;
+  private String modelo;
+  private String matricula;
+  
+  public Vehiculo(String marca, String modelo, String matricula) {
+
+    this.marca = marca;
+    this.modelo = modelo;
+    this.matricula = matricula;
+  }
+  
+  
+  
+}
